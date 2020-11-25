eeff7fe16917ecc73f7c480b835c88a903b1b314
wsboos <boos.14768@gmail.com>
2020-11-25 19:06:07 +0700

    Update eGPUbottom.vue

--- src/components/eGPUsPage/eGPUbottom.vue
+++ src/components/eGPUsPage/eGPUbottom.vue
@@ -5,23 +5,23 @@
         <div class="row d-flex justify-content-center mb-5">
           <div class="card-gpu no-border col-4 card no-padding pt-4 pl-2 pr-4">
             <img class="card-img-top" src="@/assets/products/2eGPUs/12.jpg" v-bind:alt="pic" />
-            <h3 class="text-head-bottom razerf5">VERSATILE COMPATIBILITY</h3>
-            <div class="text-description">
-              The Razer Core X external graphic enclosure
-              <br />is incredibly
-              <strong>versatile and compatible</strong> with
-              <br />
-              <strong>Thunderbolt™ 3</strong> systems running Windows
-              <br />10 RS5 or later and Macs running macOS
-              <br />High Sierra 10.13.4 or later. Laptops require
-              <br />a Thunderbolt 3 port with external graphics (eGFX) support.
-            </div>
+            <h3 class="text-head-bottom razerf5" style="text-align: left;">VERSATILE COMPATIBILITY</h3>
+              <div class="text-description razerf5" style="padding-left: 2%; text-align: left;">
+                The Razer Core X external graphic enclosure
+                <br />is incredibly
+                <strong>versatile and compatible</strong> with
+                <br />
+                <strong>Thunderbolt™ 3</strong> systems running Windows
+                <br />10 RS5 or later and Macs running macOS
+                <br />High Sierra 10.13.4 or later. Laptops require
+                <br />a Thunderbolt 3 port with external graphics (eGFX) support.
+              </div>
           </div>
 
           <div class="card-gpu no-border col-4 card no-padding pt-4 pl-2 pr-4">
             <img class="card-img-top" src="@/assets/products/2eGPUs/13.jpg" v-bind:alt="pic" />
-            <h3 class="text-head-bottom razerf5">FUTURE PROOF</h3>
-            <div class="text-description">
+            <h3 class="text-head-bottom razerf5" style="text-align: left;">FUTURE PROOF</h3>
+            <div class="text-description razerf5" style="padding-left: 2%; text-align: left;">
               Stay at the top of your game by keeping your
               <br />performance maxed. Razer Core X series
               <br />lets you easily
@@ -33,8 +33,8 @@
 
           <div class="card-gpu no-border col-4 card no-padding pt-4 pl-2 pr-4">
             <img class="card-img-top" src="@/assets/products/2eGPUs/14.jpg" v-bind:alt="pic" />
-            <h3 class="text-head-bottom razerf5">STAY COOL ALWAYS</h3>
-            <div class="text-description">
+            <h3 class="text-head-bottom razerf5" style="text-align: left;">STAY COOL ALWAYS</h3>
+            <div class="text-description razerf5" style="padding-left: 2%; text-align: left;">
               Perfectly sized to fit your setup, the Razer
               <br />Core X series feature additional cooling and
               <br />open vents in its aluminum body for
@@ -44,7 +44,7 @@
           </div>
         </div>
 
-        <p class="pt-5 mb-5" style="color: #888; font-size: 0.9em">
+        <p class="pt-5 mb-5" style="color: #888; font-size: 0.9em ">
           Razer compatible laptops include the Razer Blade Stealth, Razer Blade,
           and Razer Blade Pro (GTX 1060 version) with Thunderbolt 3 port.
           <br />Laptops require a Thunderbolt 3 port with
