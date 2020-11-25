78edc264d7559d88ee597b3d9cf3bbba6a31b9ba
PwrFr <54875724+PwrFr@users.noreply.github.com>
2020-11-25 20:43:35 +0700

    htd

--- src/components/SideImgRowR.vue
+++ src/components/SideImgRowR.vue
@@ -6,11 +6,23 @@
         backgroundImage: 'url(' + require('@/' + bgimgRR) + ')',
       }"
     >
-      <div class="ml-5 pl-5 product-access-title pl-0 pr-0 row justify-content-end">
+      <div
+        class="ml-5 pl-5 product-access-title pl-0 pr-0 row justify-content-end"
+      >
         <div class="col-4 pr-4 mr-4 pl-0 razerf5">
-          <h1 class style="font-size: 1.7rem; padding-right: 4rem">{{ title }}</h1>
-          <p class="pr-5 mr-1" style="color: #999999; font-size: 1.2rem">{{ dis }}</p>
-          <div class="m-3" style="height: 6rem; width: 6rem"></div>
+          <h1 class style="font-size: 1.7rem; padding-right: 4rem">
+            {{ title }}
+          </h1>
+          <p class="pr-5 mr-1" style="color: #999999; font-size: 1.2rem">
+            {{ dis }}
+          </p>
+          <div
+            class="m-3"
+            style="height: 6rem; width: 6rem"
+            v-bind:style="{
+              backgroundImage: 'url(' + require('@/' + probb) + ')',
+            }"
+          ></div>
         </div>
       </div>
     </div>
@@ -23,8 +35,8 @@ export default {
     bgimgRR: String,
     title: String,
     dis: String,
-    probb: String
-  }
+    probb: String,
+  },
 };
 </script>
 
