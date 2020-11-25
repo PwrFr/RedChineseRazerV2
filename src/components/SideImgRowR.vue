2625d20ffbbc028177880d5e49ebaf804f4b2545
PwrFr <54875724+PwrFr@users.noreply.github.com>
2020-11-25 19:00:00 +0700

    monitor

--- src/components/SideImgRowR.vue
+++ src/components/SideImgRowR.vue
@@ -3,26 +3,20 @@
     <div
       class="bgRL d-flex align-items-center"
       v-bind:style="{
-        backgroundImage: 'url(' + require(bgimgRR) + ')',
+        backgroundImage: 'url(' + require('@/' + bgimgRR) + ')',
       }"
     >
       <div
         class="ml-5 pl-5 product-access-title pl-0 pr-0 row justify-content-end"
       >
         <div class="col-4 pr-4 mr-4 pl-0 razerf5">
-          <h1 style="font-size: 1.7rem; padding-right: 4rem">
+          <h1 class="" style="font-size: 1.7rem; padding-right: 4rem">
             {{ title }}
           </h1>
           <p class="pr-5 mr-1" style="color: #999999; font-size: 1.2rem">
             {{ dis }}
           </p>
-          <div
-            class="m-3"
-            style="height: 6rem; width: 6rem"
-            v-bind:style="{
-              backgroundImage: 'url(' + require(probb) + ')',
-            }"
-          ></div>
+          <div class="m-3" style="height: 6rem; width: 6rem"></div>
         </div>
       </div>
     </div>
